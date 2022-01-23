520f441dabd2135f0bf6cc6195c9a15050fbca59
DavidKibetRono <david.rono@student.moringaschool.com>
2022-01-23 11:26:16 +0300

    update the file to add save and delete test cases

--- user_test.py
+++ user_test.py
@@ -21,6 +21,21 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.username,"David")
         self.assertEqual(self.new_user.password,"David@")
 
+    # Second Test --- check if it can save users
+    def test_save_user(self):
+        """test_save_user test case to test if the user object is saved into the user list"""
+
+        self.new_user.save_user()  # saving the new user
+        self.assertEqual(len(User.user_list), 1)
+
+    # Third Test --- check if it can save multiple users
+    def test_save_multiple_users(self):
+        """test_save_multiple_users to check if we can save multiple user objects to our user_list"""
+
+        self.new_user.save_user()
+        test_user = User("Mercy", "Mchelaa")  # new user
+        test_user.save_user()
+        self.assertEqual(len(User.user_list), 2)
 
     if __name__ == "__main__":
         unittest.main()
