bedc081574cac2c54a8d139f992d82323a7c2ea1
DavidKibetRono <david.rono@student.moringaschool.com>
2022-01-23 11:38:16 +0300

    update save and delete credential methods

--- credentials.py
+++ credentials.py
@@ -11,3 +11,11 @@ class Credentials:
         self.account=account
         self.username=username
         self.password=password
+
+    def save_credentials(self):
+        """save_credentials method to save save_credentials objects into use_credentials_list """
+        Credentials.use_credentials_list.append(self)
+
+    def delete_credentials(self):
+        """delete_credentials method deletes saved delete_credentials  from the use_credentials_list"""
+        Credentials.use_credentials_list.remove(self)
